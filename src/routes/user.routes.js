681e4de4a62730150486c17617382575e2a62c03
Virendra <official.vksingh01@gmail.com>
2023-12-17 23:30:42 +0530

    fixed errors

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,10 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+// import { loginUser, logoutUser, registerUser, refreshAccessToken } from "../controllers/user.controller.js";
+import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
+// import { registerUser } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+
 
 const router= Router()
 
